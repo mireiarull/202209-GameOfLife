2a6bbe7c7bc72312f78e028784b35cbd0850f3eb
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-10-08 17:06:23 +0200

    Add check neighbors function

---
+++ public/js/checkCellNeighbors/checkCellNeighbors.js
@@ -0,0 +1,28 @@
+const checkCellNeighbors = (board, cell) => {
+  const { positionX } = cell;
+  const { positionY } = cell;
+
+  let aliveNeighborsCount = 0;
+
+  const directions = {
+    top: board[positionX - 1]?.[positionY - 1] || 0,
+    topmiddle: board[positionX - 1]?.[positionY] || 0,
+    topright: board[positionX - 1]?.[positionY + 1] || 0,
+    middleleft: board[positionX]?.[positionY - 1] || 0,
+    middleright: board[positionX]?.[positionY + 1] || 0,
+    bottomleft: board[positionX + 1]?.[positionY - 1] || 0,
+    bottommiddle: board[positionX + 1]?.[positionY] || 0,
+    bottomright: board[positionX + 1]?.[positionY + 1] || 0,
+  };
+
+  const directionsArray = Object.values(directions);
+  directionsArray.forEach((direction) => {
+    if (direction.isAlive) {
+      aliveNeighborsCount++;
+    }
+  });
+
+  return aliveNeighborsCount;
+};
+
+export default checkCellNeighbors;
