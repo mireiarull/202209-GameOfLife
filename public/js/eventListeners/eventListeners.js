7d307f8c8125bcae93aa81b9f5a597e0ac3e32cb
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-10-08 21:47:45 +0200

    Add event listeners and set interval for game initialization

---
+++ public/js/eventListeners/eventListeners.js
@@ -0,0 +1,8 @@
+// import playGame from "../playGame/playGame.js";
+import startButton from "../startButton/startButton.js";
+
+const eventListeners = (board) => {
+  startButton(board);
+};
+
+export default eventListeners;
--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,3 @@
-import initialSetup from "./initialSetup/initialSetup.js";
-import setCellFutureState from "./setCellFutureState/setCellFutureState.js";
-import printBoard from "./printBoard/printBoard.js";
-import checkCellNeighbors from "./checkCellNeighbors/checkCellNeighbors.js";
-import updateCellState from "./updateCellState/updateCellState.js";
+import gameOfLife from "./gameOfLife/gameOfLife.js";
 
-const playGame = () => {
-  const board = initialSetup();
-
-  let currentGeneration = 1;
-  const lastGeneration = 10;
-
-  while (currentGeneration < lastGeneration) {
-    printBoard(board);
-    for (let xAxis = 0; xAxis < board.length; xAxis++) {
-      for (let yAxis = 0; yAxis < board[xAxis].length; yAxis++) {
-        const cell = board[xAxis][yAxis];
-        const aliveNeighbors = checkCellNeighbors(board, cell);
-        setCellFutureState(aliveNeighbors, cell);
-      }
-    }
-    updateCellState(board);
-    console.log("debugger: Next generation");
-    currentGeneration++;
-  }
-};
-
-playGame();
-
-// printBoard(board);
-// check neighbour cells -> change next cell status
-// print current state
-// change future state for current state
+gameOfLife();
